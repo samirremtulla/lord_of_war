ec41f6e373005ac1ee86e4bfbec814b833a6f8b0
Samir Remtulla <samir.remtulla@gmail.com>
2013-04-09 17:56:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LordOfWar::Application.config.secret_token = '8354339731c5c7dfd52878f112042f625e4d5b6297e9aba0f9926eba9a16886eeb5f2457c75afa59268dc32d1c35eb25eeaec997b4ac30f7e830f6372e445457'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LordOfWar::Application.config.session_store :cookie_store, key: '_lord_of_war_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LordOfWar::Application.config.session_store :active_record_store
